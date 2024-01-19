3444627f7e34cb27f89a96f01cd7f0c4534a1302
Gargameth <stragorhelm@gmail.com>
2024-01-19 13:45:57 +0100

    Add knight movement.

--- src/Chess/Figures/Knight.java
+++ src/Chess/Figures/Knight.java
@@ -1,6 +1,10 @@
 package Chess.Figures;
 
 import Chess.Board;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class Knight extends Chess.Figures.Figure {
@@ -14,11 +18,45 @@ public class Knight extends Chess.Figures.Figure {
 
     @Override
     public void move(Board board, int[] startCoords, int[] endCoords) {
-
+        boolean moveIsValid = checkMoveValidity(board, startCoords, endCoords);
+        if (moveIsValid){
+            int startIndex = board.getBoard().indexOf(board.selectFigure(startCoords));
+            int endIndex = board.getBoard().indexOf(board.selectFigure(endCoords));
+            board.getBoard().set(endIndex, this);
+            this.setCoordinate(endCoords);
+            board.getBoard().set(startIndex, new Empty(board, startCoords));
+        } else System.out.println("End coords may not be valid. Try something else.");
     }
 
     @Override
     public boolean checkMoveValidity(Board board, int[] startCoords, int[] endCoords) {
+        if (endCoords[0] < 0 || endCoords[0] > 7 || endCoords[1] < 0 || endCoords[1] > 7){
+            System.out.println("End index out of range. Pick something that's on the board.");
+            return false;
+        }
+
+        String startingColor = board.selectFigure(startCoords).getColor();
+        String endColor = board.selectFigure(endCoords).getColor();
+
+        List<Figure> possibleTargetSpaces = new ArrayList<>();
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] - 2, startCoords[1] + 1}));
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] - 1, startCoords[1] + 2}));
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] + 1, startCoords[1] + 2}));
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] + 2, startCoords[1] + 1}));
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] + 2, startCoords[1] - 1}));
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] + 1, startCoords[1] - 2}));
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] - 1, startCoords[1] - 2}));
+        possibleTargetSpaces.add(board.selectFigure(new int[]{startCoords[0] - 2, startCoords[1] - 1}));
+
+        for(Figure figure : possibleTargetSpaces){
+            if (figure == null) {
+                continue;
+            }
+            if (Arrays.equals(figure.getCoordinate(), endCoords) && !Objects.equals(startingColor, endColor)){
+                return true;
+            }
+        }
+
         return false;
     }
 }
